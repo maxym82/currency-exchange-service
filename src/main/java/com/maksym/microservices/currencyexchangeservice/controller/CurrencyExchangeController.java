e514ecc845767bd4e318e826e1381bcebc2e2189
maksym <maksym.iurchenko@gmail.com>
2019-10-21 21:59:58 -0700

    init commit

---
+++ src/main/java/com/maksym/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,21 @@
+package com.maksym.microservices.currencyexchangeservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new ExchangeValue(1003L, from, to, BigDecimal.valueOf(65));
+    }
+}
